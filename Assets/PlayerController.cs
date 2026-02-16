a795d94e93e55df781472efbe8c26b7484ead314
utkuekici <utkuekici03@gmail.com>
2026-02-16 19:02:51 +0300

    .

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -36,3 +36,43 @@ public class PlayerController : MonoBehaviour
         controls.Disable();
     }
 }
+
+
+public class ControllerDegistir : MonoBehaviour
+{
+    // Inspector'dan atayacağın yeni Animator Controller dosyası
+    public RuntimeAnimatorController yeniController; 
+
+    // Geri dönmek istersen diye eski controller'ı saklamak için
+    private RuntimeAnimatorController eskiController;
+    
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        // Başlangıçtaki controller'ı hafızaya alalım
+        eskiController = anim.runtimeAnimatorController;
+    }
+
+    void Update()
+    {
+        // 'K' tuşuna basınca yeni controller'a geç
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ControlleriGuncelle(yeniController);
+        }
+        
+        // 'L' tuşuna basınca eskiye dön
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ControlleriGuncelle(eskiController);
+        }
+    }
+
+    void ControlleriGuncelle(RuntimeAnimatorController hedefController)
+    {
+        // Animator'ün beynini tamamen değiştiriyoruz
+        anim.runtimeAnimatorController = hedefController;
+    }
+}
